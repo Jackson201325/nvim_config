c864dd4e77a10f9d25acda0c6e49121aafccdd5e
Jackson <jacksonh201325@gmail.com>
2023-06-19 21:00:15 -0400

    Delete useless comments

--- lua/jackson/plugins/toggleterm.lua
+++ lua/jackson/plugins/toggleterm.lua
@@ -27,23 +27,13 @@ function LAZYGIT_TOGGLE()
 	lazygit:toggle()
 end
 
-
 local rails_routes = terminal:new({
 	cmd = "bundle exec rails routes",
-  hidden = true,
+	hidden = true,
 	direction = "float",
 	float_opts = {
 		border = "curved",
 	},
-	-- -- function to run on opening the terminal
-	-- on_open = function(term)
-	-- 	vim.cmd("startinsert!")
-	-- 	vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
-	-- end,
-	-- -- function to run on closing the terminal
-	-- on_close = function()
-	-- 	vim.cmd("startinsert!")
-	-- end,
 })
 
 function RAILS_ROUTES_TOGGLE()
