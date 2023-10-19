1ba006d18b5079e276ba4da1ea0fe33fb265876e
Jackson <jacksonh201325@gmail.com>
2023-10-19 09:06:01 +0200

    Format changes

--- lua/jackson/plugins/lsp/mason.lua
+++ lua/jackson/plugins/lsp/mason.lua
@@ -1,67 +1,62 @@
 local servers = {
-  "astro",
-  "cssls",
-  "docker_compose_language_service",
-  "dockerls",
-  "dotls",
-  "emmet_ls",
-  "eslint",
-  "gopls",
-  "html",
-  "jsonls",
-  "lua_ls",
-  "prismals",
-  "pyright",
-  "rust_analyzer",
-  "solargraph",
-  "sqlls",
-  "tailwindcss",
-  -- "tsserver",
+	"astro",
+	"cssls",
+	"docker_compose_language_service",
+	"dockerls",
+  "elixirls",
+	"emmet_ls",
+	"eslint",
+	"html",
+	"jsonls",
+	"lua_ls",
+	"prismals",
+	"solargraph",
+	"sqlls",
+	"tailwindcss",
+	"tsserver",
 }
 
 -- IMPORTANT: make sure to setup neodev BEFORE lspconfig
-require("neodev").setup({
-  -- add any options here, or leave empty to use the default settings
-})
+require("neodev").setup({})
 
 local settings = {
-  ui = {
-    border = "none",
-    icons = {
-      package_installed = "◍",
-      package_pending = "◍",
-      package_uninstalled = "◍",
-    },
-  },
-  log_level = vim.log.levels.INFO,
-  max_concurrent_installers = 4,
+	ui = {
+		border = "none",
+		icons = {
+			package_installed = "◍",
+			package_pending = "◍",
+			package_uninstalled = "◍",
+		},
+	},
+	log_level = vim.log.levels.INFO,
+	max_concurrent_installers = 4,
 }
 
 require("mason").setup(settings)
 require("mason-lspconfig").setup({
-  ensure_installed = servers,
-  automatic_installation = true,
+	ensure_installed = servers,
+	automatic_installation = true,
 })
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status_ok then
-  return
+	return
 end
 
 local opts = {}
 
 for _, server in pairs(servers) do
-  opts = {
-    on_attach = require("jackson.plugins.lsp.handler").on_attach,
-    capabilities = require("jackson.plugins.lsp.handler").capabilities,
-  }
+	opts = {
+		on_attach = require("jackson.plugins.lsp.handler").on_attach,
+		capabilities = require("jackson.plugins.lsp.handler").capabilities,
+	}
 
-  server = vim.split(server, "@")[1]
+	server = vim.split(server, "@")[1]
 
-  local require_ok, conf_opts = pcall(require, "jackson.plugin.lsp.settings." .. server)
-  if require_ok then
-    opts = vim.tbl_deep_extend("force", conf_opts, opts)
-  end
+	local require_ok, conf_opts = pcall(require, "jackson.plugin.lsp.settings." .. server)
+	if require_ok then
+		opts = vim.tbl_deep_extend("force", conf_opts, opts)
+	end
 
-  lspconfig[server].setup(opts)
+	lspconfig[server].setup(opts)
 end
