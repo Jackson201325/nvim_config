6054606f2e49dfb548ecb055aae407f54b825fb5
Jackson <jacksonh201325@gmail.com>
2023-11-01 13:40:19 +0100

    Removed not used plugin

--- lua/jackson/plugins/nvim-ufo.lua
+++ lua/jackson/plugins/nvim-ufo.lua
@@ -48,17 +48,9 @@ vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
 vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
 vim.keymap.set("n", "zm", require("ufo").closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
--- vim.keymap.set("n", "K", function()
---   local winid = require("ufo").peekFoldedLinesUnderCursor()
---   if not winid then
---     -- choose one of coc.nvim and nvim lsp
---     vim.lsp.buf.hover()
---   end
--- end)
 
 nvim_ufo.setup({
   open_fold_hl_timeout = 150,
-  -- close_fold_kinds = { "imports", "comment" },
   close_fold_kinds = { "comment" },
   preview = {
     win_config = {
